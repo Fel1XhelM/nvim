5cf0c80358460ab119c65e1d7117cb5d31048dae
Fel1XhelM <162899172+Fel1XhelM@users.noreply.github.com>
2026-04-21 19:18:45 +0200

    Simplify shared Neovim init module loading

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
--- nvim/init.lua
--- ========================
--- Load Modules (Settings, Keymaps, Plugins, QuickUI)
--- ========================
--- require("settings")   -- General settings
-require("keymaps")    -- Keybindings
 require("settings")
-require("plugins")   -- Plugin manager & plugins
--- require("quickui")    -- QuickUI Macros
--- require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
+require("keymaps")
+require("plugins")
